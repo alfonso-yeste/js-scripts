342cbf28965f2936d9b35d3d7ce2e1038e9e181a
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2020-09-05 12:55:33 +0200

    Update menu-item wine.js

--- menu-item wine.js
+++ menu-item wine.js
@@ -1,2 +1,19 @@
 document.querySelectorAll("li[id^='menu-item-']");
 
+
+//luego hacemos una variable para que cuando alguien haga click en el parentelemnt se active
+
+document.querySelectorAll("li[id^='menu-item'] a")[0].parentElement.className
+
+
+//creamos una variable que sería tal que así:
+
+function(){
+  return {{Click Element}}.parentElement.className;
+}
+
+// y luego un activador que contenga la variable que hemos creado + contiene + menuitem
+
+//crear una etiqueta de evento ga - event - menu
+// categoria menu; acción click url; label click text
+
