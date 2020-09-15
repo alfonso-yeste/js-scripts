98ec5a567d3b53d06856e85c46c4c0d48c945e68
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2020-09-15 23:57:36 +0200

    Create params URL.js

---
+++ params URL.js
@@ -0,0 +1,14 @@
+/* la URL 
+
+https://ofertas.csmgrup.com/mazda.php?utm_source=facebook&utm_medium=cpc
+
+
+/* Primero creamos la variable/constante */
+ 
+ const params = new URLSearchParams(window.location.search);
+
+ /* Luego recogemos esos parametros de la URL en esta caso el utm_source */
+ 
+ params.get('utm_source');
+
+
